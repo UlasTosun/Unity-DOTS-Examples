30704cc628c0a7154458cb97966d3f888f465da6
Ulaş TOSUN <40580957+UlasTosun@users.noreply.github.com>
2024-12-27 22:19:06 +0300

    Default - Compute Shader case added

---
+++ Assets/Test Cases/Default - Compute Shader/DefaultComputeShaderAnimator.cs
@@ -0,0 +1,67 @@
+using UnityEngine;
+
+
+
+public class DefaultComputeShaderAnimator : AnimatorBase {
+
+    [Header("References")]
+    [Tooltip("The prefab spawner to use.")]
+    public PrefabSpawner PrefabSpawner;
+    [Tooltip("The compute shader to animate with.")]
+    public ComputeShader ComputeShader;
+
+
+
+    void Update() {
+        Transform[] objectsToAnimate = PrefabSpawner.SpawnedObjects;
+
+        Vector3[] positions = GetPositions(objectsToAnimate);
+        UpdatePositions(positions);
+        SetPositions(objectsToAnimate, positions);
+    }
+
+
+
+    private void UpdatePositions(Vector3[] positions) {
+        ComputeShader.GetKernelThreadGroupSizes(0, out uint threadsX, out uint threadsY, out uint threadsZ); // Get the thread group sizes.
+        int stride = 12; // 3 floats (Vector3) = 12 bytes.
+
+        ComputeBuffer computeBuffer = new(positions.Length, stride, ComputeBufferType.Default); // Create a compute buffer with the size of the positions array.
+        computeBuffer.SetData(positions); // Set the data of the compute buffer to the positions array.
+
+        ComputeShader.SetBuffer(0, "PositionBuffer", computeBuffer); // Set the compute buffer to the compute shader.
+
+        // Set the compute shader properties.
+        ComputeShader.SetVector("Amplitude", Amplitude);
+        ComputeShader.SetVector("Frequency", Frequency);
+        ComputeShader.SetVector("PhaseMultiplier", PhaseMultiplier);
+        ComputeShader.SetFloat("ElapsedTime", Time.time);
+
+        int threadGroupsX = Mathf.CeilToInt(positions.Length / (float)threadsX); // Calculate the number of thread groups in the X direction.
+        ComputeShader.Dispatch(0, threadGroupsX, (int)threadsY, (int)threadsZ); // Dispatch the compute shader.
+
+        computeBuffer.GetData(positions); // Get the data from the compute buffer back to the positions array.
+        computeBuffer.Release(); // Release the compute buffer after the dispatch to free up memory.
+    }
+
+
+
+    private Vector3[] GetPositions(Transform[] objectsToAnimate) {
+        Vector3[] positions = new Vector3[objectsToAnimate.Length];
+        for (int i = 0; i < objectsToAnimate.Length; i++) {
+            positions[i] = objectsToAnimate[i].position;
+        }
+        return positions;
+    }
+
+
+
+    private void SetPositions(Transform[] objectsToAnimate, Vector3[] positions) {
+        for (int i = 0; i < objectsToAnimate.Length; i++) {
+            objectsToAnimate[i].position = positions[i];
+        }
+    }
+
+
+
+}
